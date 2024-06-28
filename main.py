10ab6e03b8e6bd5b8091b87a245377e8ea8d9c47
RST2024 <rtanwani2024@gmail.com>
2024-06-28 10:02:33 -0700

    msg

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ def main():
     Code Your Program here
     ########################################
     """
+    input1 = int(input('Enter a number  '))
+    numbers.append(input1)
+    i = 0
+    while True:
+        input1 = int(input('Enter a number  '))
+        if input1 < numbers[i]:
+            numbers.append(input1)
+        else:
+            break
+        i = i + 1
 
     ########################################
     # Do not delete the return statement
